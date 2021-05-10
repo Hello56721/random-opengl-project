215f0f7b9456177fe4d3034df4e9da166557b629
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-10 16:17:26 -0400

    Installed GLFW

---
+++ cmake/FindGLFW.cmake
@@ -0,0 +1,16 @@
+find_path(
+    GLFW_INCLUDE_DIR
+    NAMES "GLFW/glfw3.h"
+    DOC "Absolute path to GLFW'is include directory"
+    PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/include"
+)
+
+find_library(
+    GLFW_LIBRARY
+    NAMES "libglfw3.a" "libglfw3dll.a" "libglfw.so" "glfw3.lib" "glfw3dll.lib"
+    DOC "Absolute path to GLFW's library"
+    PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/src" "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+)
+
+include("FindPackageHandleStandardArgs")
+find_package_handle_standard_args("GLFW" DEFAULT_MSG GLFW_LIBRARY GLFW_INCLUDE_DIR)
