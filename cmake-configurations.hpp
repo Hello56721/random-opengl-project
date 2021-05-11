79db37e07737bea1c678ce6c5097439a3f032cd7
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-11 18:15:35 -0400

    Added header guards

--- cmake-configurations.hpp
+++ cmake-configurations.hpp
@@ -1,6 +1,12 @@
+#ifndef F05A2600_83E5_41AF_8EB4_F9D12F9F61F9
+#define F05A2600_83E5_41AF_8EB4_F9D12F9F61F9
+
 // Fix something with cmake
 #define ON 1
 #define OFF 0
 
 // Wether to enable OpenGL debugging or not
 #define ENABLE_OPENGL_DEBUGGING @ENABLE_OPENGL_DEBUGGING@
+
+
+#endif /* F05A2600_83E5_41AF_8EB4_F9D12F9F61F9 */
