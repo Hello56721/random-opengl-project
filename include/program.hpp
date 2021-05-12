8b19879020c244a5bd16732b2d7cc2f1d3f80570
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-12 14:29:17 -0400

    Added program.cpp

--- include/program.hpp
+++ include/program.hpp
@@ -1,6 +1,14 @@
 #ifndef BAEB39DC_65D5_4024_9D54_9513E4A2ECCC
 #define BAEB39DC_65D5_4024_9D54_9513E4A2ECCC
 
-
+// The class that will represent the whole program
+class Program {
+public:
+    Program();
+    
+    void update();
+    
+    ~Program();
+};
 
 #endif /* BAEB39DC_65D5_4024_9D54_9513E4A2ECCC */
