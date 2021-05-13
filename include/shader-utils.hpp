6703a6345e8cf3bdb8bea0d07fd21a3a3060b121
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-13 14:36:56 -0400

    Included string_view

--- include/shader-utils.hpp
+++ include/shader-utils.hpp
@@ -1,6 +1,8 @@
 #ifndef C0226920_2CC9_46E4_90BC_5D31653C0222
 #define C0226920_2CC9_46E4_90BC_5D31653C0222
 
+#include <string_view>
+
 namespace ShaderUtils {
     // Create a shader object and return it's handle
     unsigned int createShader(const char* source, unsigned int type, std::string_view name);
