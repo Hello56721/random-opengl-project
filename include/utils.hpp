bc215ebb5594a76e22b7116d4b741bab6a7b1201
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-13 14:54:21 -0400

    Added a function to check if one string starts with another string

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,5 +1,9 @@
 #ifndef E902CCF4_15D4_47B3_90DE_F368A360C2EA
 #define E902CCF4_15D4_47B3_90DE_F368A360C2EA
 
+namespace Utils {
+    // Check if main starts with sub
+    bool startsWith(std::string_view main, std::string_view sub);
+}
 
 #endif /* E902CCF4_15D4_47B3_90DE_F368A360C2EA */
