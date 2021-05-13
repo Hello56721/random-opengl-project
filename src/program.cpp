d70ebdbe17cd9ebff0d25bbd473e92051a7fda2a
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-13 16:07:59 -0400

    Rendered a yellow rectangle.

--- src/program.cpp
+++ src/program.cpp
@@ -3,10 +3,15 @@
 #include <window.hpp>
 #include <iostream>
 #include <shader-utils.hpp>
+#include <vector>
 
 #include <program.hpp>
 
 static unsigned int shaderProgram;
+
+static unsigned int vao;
+static unsigned int vbo;
+static unsigned int ebo;
 
 Program::Program() {
     std::cout << "[INFO]: Hello!" << std::endl;
@@ -22,6 +27,32 @@ Program::Program() {
     glCall(glDeleteShader, vertexShader);
     glCall(glDeleteShader, fragmentShader);
     
+    glCall(glGenVertexArrays, 1, &vao);
+    glCall(glBindVertexArray, vao);
+    
+    float vertices[] =  {
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+    
+    glCall(glGenBuffers, 1, &vbo);
+    glCall(glBindBuffer, GL_ARRAY_BUFFER, vbo);
+    glCall(glBufferData, GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    unsigned int indices[] {
+        0, 1, 2,
+        0, 3, 2
+    };
+    
+    glCall(glGenBuffers, 1, &ebo);
+    glCall(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glCall(glBufferData, GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glCall(glVertexAttribPointer, 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glCall(glEnableVertexAttribArray, 0);
+    
     glfwShowWindow(Window::window);
 }
 
@@ -32,6 +63,9 @@ void Program::update() {
     
     glCall(glUseProgram, shaderProgram);
     
+    glCall(glBindVertexArray, vao);
+    glCall(glDrawElements, GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    
     glfwSwapBuffers(Window::window);
     glfwPollEvents();
 }
@@ -39,5 +73,10 @@ void Program::update() {
 
 
 Program::~Program() {
+    glCall(glDeleteProgram, shaderProgram);
+    glCall(glDeleteBuffers, 1, &vbo);
+    glCall(glDeleteBuffers, 1, &ebo);
+    glCall(glDeleteVertexArrays, 1, &vao);
+    
     glfwTerminate();
 }
