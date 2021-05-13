135b857fc9596738687ff82ca3a9d79933427218
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-13 14:34:36 -0400

    Added OpenGL shader utils functions

--- include/shader-utils.hpp
+++ include/shader-utils.hpp
@@ -1,5 +1,12 @@
 #ifndef C0226920_2CC9_46E4_90BC_5D31653C0222
 #define C0226920_2CC9_46E4_90BC_5D31653C0222
 
+namespace ShaderUtils {
+    // Create a shader object and return it's handle
+    unsigned int createShader(const char* source, unsigned int type, std::string_view name);
+    
+    // Create a shader program and return it's handle
+    unsigned int createProgram(unsigned int vertex, unsigned int fragment, std::string_view name);
+}
 
 #endif /* C0226920_2CC9_46E4_90BC_5D31653C0222 */
