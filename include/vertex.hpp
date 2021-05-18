a88184e27f6e8df0ab9c8efcaffbb0501891d1bf
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-18 08:24:56 -0400

    Added a struct to represent vertices

---
+++ include/vertex.hpp
@@ -0,0 +1,10 @@
+#ifndef F1CACCB3_041B_4602_B9B3_C3444C93E083
+#define F1CACCB3_041B_4602_B9B3_C3444C93E083
+
+#include <glm/glm.hpp>
+
+struct Vertex {
+    glm::vec3 position;
+};
+
+#endif /* F1CACCB3_041B_4602_B9B3_C3444C93E083 */
