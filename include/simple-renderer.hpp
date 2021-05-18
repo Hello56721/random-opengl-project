d65b8aa6995f9a2e4585ad3328548daa8de0db20
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-18 10:26:15 -0400

    Added a simple renderer to render meshes.

---
+++ include/simple-renderer.hpp
@@ -0,0 +1,10 @@
+#ifndef EE55C543_6CFE_4E6B_9436_5C65ACEA167B
+#define EE55C543_6CFE_4E6B_9436_5C65ACEA167B
+
+#include <mesh.hpp>
+
+namespace SimpleRenderer {
+    void renderMesh(Mesh& mesh);
+}
+
+#endif /* EE55C543_6CFE_4E6B_9436_5C65ACEA167B */
