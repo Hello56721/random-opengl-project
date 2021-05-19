05ec067ebec1797b3ad89cd580c3d0d8c84659d9
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-19 17:53:09 -0400

    Moved the string_view include to the header file

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,8 @@
 #ifndef E902CCF4_15D4_47B3_90DE_F368A360C2EA
 #define E902CCF4_15D4_47B3_90DE_F368A360C2EA
 
+#include <string_view>
+
 namespace Utils {
     // Check if main starts with sub
     bool startsWith(std::string_view main, std::string_view sub);
