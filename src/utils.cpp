6f43f49a7a3c8c58a04d5bdfdf96ee8543e2a975
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-20 16:01:11 -0400

    Fixed a problem that shows up when building with msvc Checked whether main is larger than sub before checking if it starts with it.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,10 @@
 #include <utils.hpp>
 
 bool Utils::startsWith(std::string_view main, std::string_view sub) {
+    if (main.length() < sub.length()) {
+        return false;
+    }
+    
     // Loop through the entire sub string from the start
     for (std::string_view::iterator i = sub.begin(), j = main.begin(); i != sub.end(); i++, j++) {
         if (*i != *j) {
