4bfbc52c401a621c950d0b87877e3d4f31848d54
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-22 14:42:33 -0400

    Implemented the shader manager

---
+++ src/shader-manager.cpp
@@ -0,0 +1,15 @@
+#include <shader-manager.hpp>
+
+#include <glad/glad.h>
+#include <opengl-debug.hpp>
+
+ShaderManager& ShaderManager::getInstance() {
+    static ShaderManager instance;
+    return instance;
+}
+
+ShaderManager::~ShaderManager() {
+    for (std::vector<Shader>::iterator i = shaders.begin(); i != shaders.end(); i++) {
+        glCall(glDeleteShader, i->program);
+    }
+}
