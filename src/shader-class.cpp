4d4eff07e97424e47e4bb094cb0bd797887460d2
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-22 14:32:26 -0400

    Implemented the shader class

---
+++ src/shader-class.cpp
@@ -0,0 +1,33 @@
+#include <shader-class.hpp>
+
+#include <shader-utils.hpp>
+#include <glad/glad.h>
+#include <opengl-debug.hpp>
+#include <shader-manager.hpp>
+
+Shader::Shader(): program(0) {
+    
+}
+
+Shader::Shader(std::string_view path) {
+    ShaderUtils::ShaderSources sources = ShaderUtils::processShader(path);
+    unsigned int vertex = ShaderUtils::createShader(sources.vertex, GL_VERTEX_SHADER, path);
+    unsigned int fragment = ShaderUtils::createShader(sources.fragment, GL_FRAGMENT_SHADER, path);
+    program = ShaderUtils::createProgram(vertex, fragment, path);
+    
+    // Delete the shaders because they are already linked to the shader program and are therefore uneeded.
+    glCall(glDeleteShader, vertex);
+    glCall(glDeleteShader, fragment);
+    
+    // Add this object to the whole list
+    ShaderManager& manager = ShaderManager::getInstance();
+    manager.shaders.push_back(*this);
+}
+
+void Shader::use() const {
+    glCall(glUseProgram, program);
+}
+
+void Shader::setUniform(std::string_view name, float value) const {
+    glCall(glUniform1f, glCallR(glGetUniformLocation, name.data(), program), value);
+}
