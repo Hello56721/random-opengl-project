dab3dd0269aa70f955b5c692bc657332772c3a36
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-05-22 14:31:56 -0400

    Made the methods that needs to be const const

--- include/shader-class.hpp
+++ include/shader-class.hpp
@@ -15,10 +15,10 @@ public:
     Shader(std::string_view path);
     
     // Use/Activate the shader
-    void use();
+    void use() const;
     
     // Set uniforms
-    void setUniform(std::string_view name, float value);
+    void setUniform(std::string_view name, float value) const;
 };
 
 #endif /* D563D817_6A65_4428_9FAE_F01285C37BA5 */
